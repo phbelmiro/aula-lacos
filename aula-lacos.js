1f6cfaf92d986d5c1c1fecac4966f2230da1018f
phbelmiro <142456085+phbelmiro@users.noreply.github.com>
2023-09-18 20:54:48 -0300

    Create aula-lacos.js

---
+++ aula-lacos.js
@@ -0,0 +1,70 @@
+/*
+pedro henrique
+
+1
+
+let valor = 0 
+for(let 1 = 0 ; i<5 ; i++){
+    valor += i  
+}s
+console.log(valor) // 10
+
+
+2
+
+A)todos numeros maiores do que 18
+
+B)
+
+3
+
+
+
+*/
+
+
+// //1)
+
+// let pet = Number(prompt("quantos bixinhos voce tem?"));
+
+// if( pet === 0) {
+//     console.log("Que pena! Você pode adotar um pet")
+// } else {
+//     while (pet !== 0) {
+//         nomesDoPet = [];
+//         resp = prompt("Qual e o nome dele?")
+//         nomesDoPet = resp
+        
+//         console.log(nomesDoPetpet)
+//     }
+// }
+
+
+
+
+//2
+
+// const arrayOriginal = [52, 12, 2, 3, 53, 24, 245, 85, 94]
+
+// for (let i = 0; i < arrayOriginal.length; i++) {
+//     const element = arrayOriginal[i];
+//     console.log(element)
+// }
+
+
+// function divisao(vet) {
+//     for(let i = 0; i< arrayOriginal.length ; i++){
+//     const element =  arrayOriginal[i]
+//     if (element / 10){
+
+//     }
+
+//     }
+
+    
+// }
+
+// console.log(divisao(arrayOriginal))
+
+
+
